4838c4a8cbfa1d17970e19503a0842cc43f762a3
Cedric-Arizobal <ced.arizobal@gmail.com>
2026-01-22 11:22:14 +0800

    Sidebar Module ID changes

--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -32,72 +32,95 @@ const navigation = [
     name: 'Disbursement',
     icon: DisbursementIcon,
     submenu: [
-      { name: 'Obligation Request',       href: '/disbursement/obligation-requests',    moduleId: 62, },
-      { name: 'Disbursement Voucher',     href: '/disbursement/vouchers',               moduleId: 40, },
-      { name: 'Cheque Generator',         href: '/disbursement/cheque-generator',       moduleId: 40, },
-      { name: 'Journal Entry Voucher',    href: '/disbursement/journal-entry-vouchers', moduleId: 57, },
-      { name: 'Purchase Request',         href: '/disbursement/purchase-requests',      moduleId: 69, },
-      { name: 'Fund Utilization Request', href: '/disbursement/fund-utilization-requests',  moduleId: 47, },
-      { name: 'Beginning Balance',        href: '/disbursement/beginning-balance',      moduleId: 21, },
-      { name: 'Travel Order',             href: '/disbursement/travel-orders',          moduleId: 80, },
-      {
-        name: 'Reports',
-        submenu: [
-          // { name: 'Disbursement Journals',  href: '/disbursement/disbursement-journals',  moduleId: 32, }, // Check & Cash Disbursement?
-          // { name: 'General Journals',       href: '/disbursement/general-journals',       moduleId: 49, },
-          // { name: 'General Ledger',         href: '/reports/general-ledger',              moduleId: 50, },
-          // { name: 'Subsidiary Ledger',      href: '/reports/subsidiary-ledger',           moduleId: 77, },
-          // { name: 'Trial Balance',          href: '/reports/trial-balance',               moduleId: 81, },
-          // { name: 'Financial Statements',   href: '/reports/financial-statements' }, // no mapping
-          // { name: 'BIR Reports',            href: '/reports/bir',                         moduleId: 16, },
-          { name: 'Nothing'}
-        ]
-      }
+      { name: 'Obligation Request',       href: '/disbursement/obligation-requests',        moduleId: 1, },
+      { name: 'Disbursement Voucher',     href: '/disbursement/vouchers',                   moduleId: 2, },
+      { name: 'Cheque Generator',         href: '/disbursement/cheque-generator',           moduleId: 3, },
+      { name: 'Journal Entry Voucher',    href: '/disbursement/journal-entry-vouchers',     moduleId: 4, },
+      { name: 'Purchase Request',         href: '/disbursement/purchase-requests',          moduleId: 5, },
+      { name: 'Fund Utilization Request', href: '/disbursement/fund-utilization-requests',  moduleId: 6, },
+      { name: 'Beginning Balance',        href: '/disbursement/beginning-balance',          moduleId: 7, },
+      { name: 'Travel Order',             href: '/disbursement/travel-orders',              moduleId: 8, },
+      // {
+      //   name: 'Reports',
+      //   submenu: [
+      //     { name: 'Disbursement Journals',  href: '/disbursement/disbursement-journals',  moduleId: 32, }, // Check & Cash Disbursement?
+      //     { name: 'General Journals',       href: '/disbursement/general-journals',       moduleId: 49, },
+      //     { name: 'General Ledger',         href: '/reports/general-ledger',              moduleId: 50, },
+      //     { name: 'Subsidiary Ledger',      href: '/reports/subsidiary-ledger',           moduleId: 77, },
+      //     { name: 'Trial Balance',          href: '/reports/trial-balance',               moduleId: 81, },
+      //     { name: 'Financial Statements',   href: '/reports/financial-statements' }, // no mapping
+      //     { name: 'BIR Reports',            href: '/reports/bir',                         moduleId: 16, },
+      //     { name: 'Nothing'}
+      //   ]
+      // }
     ],
   },
   {
     name: 'Collections',
     icon: CollectionsIcon,
     submenu: [
-      { name: 'Community Tax Individual',   href: '/collections/community-tax',             moduleId: 34, },
-      { name: 'Community Tax Corporate',    href: '/collections/community-tax-corporation', moduleId: 35, },
-      { name: 'General Service Invoice',    href: '/collections/general-service-receipts',  moduleId: 52, },
-      { name: 'Burial Service Receipt',     href: '/collections/burial-service-receipts',   moduleId: 28, },
-      { name: 'Marriage Service Receipt',   href: '/collections/marriage-service-receipts', moduleId: 59, },
-      { name: 'Real Property Tax Receipt',  href: '/collections/real-property-tax',         moduleId: 70, },
-      { name: 'Public Market Ticket',       href: '/collections/public-market-tickets',     moduleId: 68, },
-      { name: 'Business Permits',           href: '/collections/business-permits',          moduleId: 29, },
-      {
-        name: 'Reports',
-        submenu: [
-          // { name: 'Cashbook', href: '/collections/cashbook', moduleId: 30 },
-          // { name: 'Collection Report', href: '/collections/reports' },
-          { name: 'Nothing'}
-        ]
-      }
+      { name: 'Community Tax Individual',   href: '/collections/community-tax',             moduleId: 9, },
+      { name: 'Community Tax Corporate',    href: '/collections/community-tax-corporation', moduleId: 10, },
+      { name: 'General Service Invoice',    href: '/collections/general-service-receipts',  moduleId: 11, },
+      { name: 'Burial Service Receipt',     href: '/collections/burial-service-receipts',   moduleId: 12, },
+      { name: 'Marriage Service Receipt',   href: '/collections/marriage-service-receipts', moduleId: 13, },
+      { name: 'Real Property Tax Receipt',  href: '/collections/real-property-tax',         moduleId: 14, },
+      { name: 'Public Market Ticket',       href: '/collections/public-market-tickets',     moduleId: 15, },
+      { name: 'Business Permits',           href: '/collections/business-permits',          moduleId: 16, },
+      // {
+      //   name: 'Reports',
+      //   submenu: [
+      //     { name: 'Cashbook', href: '/collections/cashbook', moduleId: 30 },
+      //     { name: 'Collection Report', href: '/collections/reports' },
+      //     { name: 'Nothing'}
+      //   ]
+      // }
     ],
   },
   {
     name: 'Budget',
     icon: BudgetIcon,
     submenu: [
-      { name: 'Funds',                href: '/budget/funds',          moduleId: 48 },
+      { name: 'Funds',                href: '/budget/funds',          moduleId: 17 },
       // { name: 'Sub-funds',            href: '/budget/sub-funds',      moduleId: 97 },
-      { name: 'Budget Appropriation', href: '/budget/details',        moduleId: 22 },
-      { name: 'Budget Allotment',     href: '/budget/allotment',      moduleId: 23 },
-      { name: 'Budget Supplemental',  href: '/budget/supplemental',   moduleId: 26 },
-      { name: 'Budget Transfer',      href: '/budget/transfer',       moduleId: 27 },
-      { name: 'Fund Transfer',        href: '/budget/fund-transfer',  moduleId: 46 },
-      {
-        name: 'Reports',
-        submenu: [
-          // { name: 'Budget Report',              href: '/budget/report',                  moduleId: 24 },
-          // { name: 'Statement of Comparison',    href: '/budget/statement-comparison',    moduleId: 73 },
-          // { name: 'Statement of Appropriation', href: '/budget/statement-appropriation', moduleId: 72 },
-          { name: 'Budget Summary', href: '/budget/summary',  moduleId: 25 },
-          { name: 'And Nothing'}
-        ] 
-      },
+      { name: 'Budget Appropriation', href: '/budget/details',        moduleId: 18 },
+      { name: 'Budget Allotment',     href: '/budget/allotment',      moduleId: 19 },
+      { name: 'Budget Supplemental',  href: '/budget/supplemental',   moduleId: 20 },
+      { name: 'Budget Transfer',      href: '/budget/transfer',       moduleId: 21 },
+      { name: 'Fund Transfer',        href: '/budget/fund-transfer',  moduleId: 22 },
+      // {
+      //   name: 'Reports',
+      //   submenu: [
+      //     { name: 'Budget Report',              href: '/budget/report',                  moduleId: 24 },
+      //     { name: 'Statement of Comparison',    href: '/budget/statement-comparison',    moduleId: 73 },
+      //     { name: 'Statement of Appropriation', href: '/budget/statement-appropriation', moduleId: 72 },
+      //     { name: 'Budget Summary', href: '/budget/summary',  moduleId: 25 },
+      //     { name: 'And Nothing'}
+      //   ] 
+      // },
+    ],
+  },
+  {
+    name: 'Reports',
+    icon: ChartBar,
+    submenu: [
+      { name: 'Disbursement Journals',      href: '/disbursement/disbursement-journals',  moduleId: 23, }, // Check & Cash Disbursement?
+      { name: 'General Ledger',             href: '/reports/general-ledger',              moduleId: 24, },
+      { name: 'Subsidiary Ledger',          href: '/reports/subsidiary-ledger',           moduleId: 25, },
+      { name: 'Trial Balance',              href: '/reports/trial-balance',               moduleId: 26, },
+      { name: 'Post-Closing',               href: '/reports/post-closing',                moduleId: 27, },
+      { name: 'Financial Position',         href: '/reports/financial-position',          moduleId: 28, },
+      { name: 'Financial Performance',      href: '/reports/financial-performance',       moduleId: 29, },
+      { name: 'Cash Flow',                  href: '/reports/cash-flow',                   moduleId: 30, },
+      { name: 'Change in Equity',           href: '/reports/change-in-equity',            moduleId: 31, },
+      { name: 'BIR Reports',                href: '/reports/bir',                         moduleId: 32, },
+      { name: 'Alphalist',                  href: '/reports/alphalist',                   moduleId: 33, },
+      { name: 'Cashbook',                   href: '/collections/cashbook',                moduleId: 34, },
+      { name: 'Collection Report',          href: '/collections/reports',                 moduleId: 35, },
+      { name: 'Budget Summary',             href: '/budget/summary',                      moduleId: 36, },
+      { name: 'Statement of Comparison',    href: '/budget/statement-comparison',         moduleId: 37, },
+      { name: 'Statement of Appropriation', href: '/budget/statement-appropriation',      moduleId: 38, },
+
     ],
   },
   {
@@ -105,32 +128,32 @@ const navigation = [
     icon: SettingsIcon,
     submenu: [
       { name: 'Departments',        href: '/settings/departments',        moduleId: 39, },
-      { name: 'Subdepartments',     href: '/settings/subdepartments',     moduleId: 76, },
-      { name: 'LGU Maintenance',    href: '/settings/lgu-maintenance',    moduleId: 58, },
-      { name: 'Locations',          href: '/settings/locations' }, // no mapping DONE
+      { name: 'Subdepartments',     href: '/settings/subdepartments',     moduleId: 40, },
+      { name: 'LGU Maintenance',    href: '/settings/lgu-maintenance',    moduleId: 41, },
+      { name: 'Locations',          href: '/settings/locations',          moduleId: 42, }, // no mapping DONE
       {
         name: 'User Settings',
         submenu: [
-          { name: 'User Details', href: '/settings/users',       moduleId: 82, },
-          { name: 'User Roles',   href: '/settings/user-roles',  moduleId: 83, },
-          { name: 'Role Access',  href: '/settings/role-access', moduleId: 84, },
+          { name: 'User Details', href: '/settings/users',       moduleId: 43, },
+          { name: 'User Roles',   href: '/settings/user-roles',  moduleId: 44, },
+          { name: 'Role Access',  href: '/settings/role-access', moduleId: 45, },
         ],
       },
       {
         name: 'Employee Settings',
         submenu: [
-          { name: 'Employee Details',   href: '/settings/employees',        moduleId: 43, },
-          { name: 'Employment Status',  href: '/settings/employmentStatus', moduleId: 93, },
-          { name: 'Positions',          href: '/settings/positions',        moduleId: 36, },
-          { name: 'Nationalities',      href: '/settings/nationalities',    moduleId: 94, },
-          { name: 'Approval Matrix',    href: '/settings/approval-matrix',  moduleId: 17, },
+          { name: 'Employee Details',   href: '/settings/employees',        moduleId: 46, },
+          { name: 'Employment Status',  href: '/settings/employmentStatus', moduleId: 47, },
+          { name: 'Positions',          href: '/settings/positions',        moduleId: 48, },
+          { name: 'Nationalities',      href: '/settings/nationalities',    moduleId: 49, },
+          { name: 'Approval Matrix',    href: '/settings/approval-matrix',  moduleId: 50, },
         ],
       },
       {
         name: 'Partner Settings',
         submenu: [
-          { name: 'Vendors',      href: '/settings/vendors',  moduleId: 86, },
-          { name: 'Individuals',  href: '/settings/customer', moduleId: 38, },
+          { name: 'Vendors',      href: '/settings/vendors',  moduleId: 51, },
+          { name: 'Individuals',  href: '/settings/customer', moduleId: 52, },
         ],
       },
       {
@@ -139,71 +162,72 @@ const navigation = [
           {
             name: 'Bank Details',
             submenu: [
-              { name: 'Bank',     href: '/settings/bank',     moduleId: 18 },
-              { name: 'Currency', href: '/settings/currency', moduleId: 37 },
+              { name: 'Bank',     href: '/settings/bank',     moduleId: 53 },
+              { name: 'Currency', href: '/settings/currency', moduleId: 54 },
             ],
           },
           {
             name: 'Document Details',
             submenu: [
-              { name: 'Document Type',          href: '/settings/document-details',         moduleId: 41, },
-              { name: 'Document Type Category', href: '/settings/document-type-categories', moduleId: 42, },
+              { name: 'Document Type',          href: '/settings/document-details',         moduleId: 55, },
+              { name: 'Document Type Category', href: '/settings/document-type-categories', moduleId: 56, },
             ],
           },
           {
             name: 'Items Settings',
             submenu: [
-              { name: 'Item List',  href: '/settings/items',        moduleId: 55 },
-              { name: 'Item Units', href: '/settings/items/units',  moduleId: 56, },
+              { name: 'Item List',  href: '/settings/items',        moduleId: 57 },
+              { name: 'Item Units', href: '/settings/items/units',  moduleId: 58, },
             ],
           },
           {
             name: 'Project Settings',
             submenu: [
-              { name: 'Project Details',  href: '/settings/project-details',  moduleId: 65, },
-              { name: 'Project Type',     href: '/settings/project-type',     moduleId: 87, },
+              { name: 'Project Details',  href: '/settings/project-details',  moduleId: 59, },
+              { name: 'Project Type',     href: '/settings/project-type',     moduleId: 60, },
             ],
           },
-          { name: 'Financial Statement',  href: '/settings/financial-statement',  moduleId: 44, },
-          { name: 'Fiscal Year',          href: '/settings/fiscal-year',          moduleId: 45, },
-          { name: 'Tax Code',             href: '/settings/tax-code',             moduleId: 78, },
-          { name: 'Mode of Payment',      href: '/settings/mode-of-payment',      moduleId: 60, },
-          { name: 'Payment Terms',        href: '/settings/payment-terms',        moduleId: 63, },
-          { name: 'Industry',             href: '/settings/industry',             moduleId: 53, },
+          // { name: 'Financial Statement',  href: '/settings/financial-statement',  moduleId: 44, },
+          { name: 'Fiscal Year',          href: '/settings/fiscal-year',          moduleId: 61, },
+          { name: 'Tax Code',             href: '/settings/tax-code',             moduleId: 62, },
+          { name: 'Mode of Payment',      href: '/settings/mode-of-payment',      moduleId: 63, },
+          { name: 'Payment Terms',        href: '/settings/payment-terms',        moduleId: 64, },
+          { name: 'Industry',             href: '/settings/industry',             moduleId: 65, },
         ],
       },
       {
         name: 'Chart of Accounts Settings',
         submenu: [
-          { name: 'Chart of Accounts',        href: '/settings/chart-of-accounts',        moduleId: 31, },
-          { name: 'Account Group',            href: '/settings/account-group',            moduleId: 88, },
-          { name: 'Major Account Group',      href: '/settings/major-account-group',      moduleId: 89, },
-          { name: 'Sub Major Account Group',  href: '/settings/sub-major-account-group',  moduleId: 90, },
+          { name: 'Account Group',            href: '/settings/account-group',            moduleId: 66, },
+          { name: 'Major Account Group',      href: '/settings/major-account-group',      moduleId: 67, },
+          { name: 'Sub Major Account Group',  href: '/settings/sub-major-account-group',  moduleId: 68, },
+          { name: 'Chart of Accounts',        href: '/settings/chart-of-accounts',        moduleId: 69, },
+          { name: 'Subsidiary Accounts',      href: '/settings/subsidiary-accounts',      moduleId: 70, },
         ],
       },
       {
         name: 'Report Settings',
         submenu: [
-          { name: 'Signatories',      href: '/settings/signatories',      moduleId: 0, },
-          { name: 'Logos and Images', href: '/settings/logos-and-images', moduleId: 0, },
-          { name: 'Watermarks',       href: '/settings/watermarks',       moduleId: 0, },
-          { name: 'Data Sources',     href: '/settings/data-sources',     moduleId: 0, },
+          { name: 'Signatories',      href: '/settings/signatories',      moduleId: 71, },
+          { name: 'Logos and Images', href: '/settings/logos-and-images', moduleId: 72, },
+          { name: 'Watermarks',       href: '/settings/watermarks',       moduleId: 73, },
+          { name: 'Data Sources',     href: '/settings/data-sources',     moduleId: 74, },
         ],
       },
       {
         name: 'Real Property Settings',
         submenu: [
-          { name: 'Tax Declaration',    href: '/settings/tax-declaration',    moduleId: 79, },
-          { name: 'Base Unit Value',    href: '/settings/base-unit-value',    moduleId: 20, },
-          { name: 'General Revision',   href: '/settings/general-revision',   moduleId: 51, },
+          { name: 'Tax Declaration',    href: '/settings/tax-declaration',    moduleId: 75, },
+          { name: 'Base Unit Value',    href: '/settings/base-unit-value',    moduleId: 76, },
+          { name: 'General Revision',   href: '/settings/general-revision',   moduleId: 77, },
         ],
       },
       {
         name: 'PPE Settings',
         submenu: [
-          { name: 'PPE List',         href: '/settings/ppe',            moduleId: 64, },
-          { name: 'PPE Categories',   href: '/settings/ppe-categories', moduleId: 95, },
-          { name: 'PPE Suppliers',    href: '/settings/ppe-suppliers',  moduleId: 96, },
+          { name: 'PPE List',         href: '/settings/ppe',            moduleId: 78, },
+          { name: 'PPE Categories',   href: '/settings/ppe-categories', moduleId: 79, },
+          { name: 'PPE Suppliers',    href: '/settings/ppe-suppliers',  moduleId: 80, },
         ],
       },
     ],
